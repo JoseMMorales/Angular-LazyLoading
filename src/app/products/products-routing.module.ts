1a3c991ae78280fefef84e3db91c4abbb1d8ebc2
JoseMMorales <josemulamorales33@hotmail.com>
2021-12-22 16:34:59 +0100

    Adding routes path to products module

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './pages/add/add.component';
+import { EditComponent } from './pages/edit/edit.component';
+import { ListComponent } from './pages/list/list.component';
+import { ProductComponent } from './pages/product/product.component';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    children: [
+      { path: 'add', component: AddComponent },
+      { path: 'edit', component: EditComponent },
+      { path: 'list', component: ListComponent },
+      { path: 'product', component: ProductComponent },
+      { path: '**', redirectTo: 'list' }
+    ]
+  }
 ];
 
 @NgModule({
