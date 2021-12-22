26514d1584ae94742f3ebfe01982fbe47d0d32c5
JoseMMorales <josemulamorales33@hotmail.com>
2021-12-22 16:04:58 +0100

    Creating login, password, register

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PasswordComponent } from './pages/password/password.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterComponent,
+    LoginComponent,
+    PasswordComponent
+  ],
   imports: [
     CommonModule
   ]
