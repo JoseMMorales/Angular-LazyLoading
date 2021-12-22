467806cf8d35fd75580a6dfa4803989952967eb5
JoseMMorales <josemulamorales33@hotmail.com>
2021-12-22 16:48:13 +0100

    Creating users pages as edit, list, new and user

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { EditComponent } from './pages/edit/edit.component';
+import { ListComponent } from './pages/list/list.component';
+import { NewComponent } from './pages/new/new.component';
+import { UserComponent } from './pages/user/user.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EditComponent,
+    ListComponent,
+    NewComponent,
+    UserComponent
+  ],
   imports: [
     CommonModule,
     UsersRoutingModule
